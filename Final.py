f082253c84552beb8f1d79973fdb967e384aaec5
072739626 <50999959+072739626@users.noreply.github.com>
2019-05-28 14:48:39 -0400

    Update Final.py
    Solver functions start

--- Final.py
+++ Final.py
@@ -1,3 +1,34 @@
 import math
 import python
 
+#Solver funtions
+
+class Vector(x,y,z):
+
+    def __init__(self,x,y,z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+# Cross product function
+
+def cross(vec1,vec2):
+    return (vec1.y*vec2.z - vec1.z*vec2.y, vec1.z*vec2.x - vec1.x*vec2.z, vec1.x*vec2.y - vec1.y*vec2.x)
+    
+
+#2 Planes
+
+def twoPlanes(plane1,plane2):
+    if plane1.normal == plane2.normal:
+        if plane1.eqn == plane2.eqn:
+            return plane1
+        else:
+            return None
+    else:
+        #Need calculations here to determine the point and the direction vector
+        return line(dirVec, point)
+
+#3 Planes
+
+def threePlanes(plane1,plane2,plane3):
+
