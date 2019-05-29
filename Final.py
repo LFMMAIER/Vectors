d5c75fe1869e5fa78860afebde2a5c940666a876
072586076 <50999997+072586076@users.noreply.github.com>
2019-05-29 14:27:19 -0400

    Update Final.py

--- Final.py
+++ Final.py
@@ -49,3 +49,79 @@ def isPointPlane(point1,plane1):
         return True
     else:
         return False
+    
+#Equation
+
+class Line():
+
+    #point cords in space (x,y,z) or (x,y)
+    #dirVector a given dirction from a point (x,y,z) or (x,y)
+    #space either '2d' or '3d'
+    def __init__(self,point,dirVector,space):
+
+        self.point = point
+        if space == '2d':
+            self.dirVector = Vector(dirVector[0],dirVector[1],0)
+        else:
+            self.dirVector = Vector(dirVector[0],dirVector[1],dirVector[2])
+        self.space = space
+        self.normal = Line.norm(dirVector,space)
+
+    #retuns the normal vector of the line
+    @staticmethod
+    def norm(dirVector,space):
+        if space == '2d':
+            normal = Vector(dirVector[1],dirVector[0],0)
+            return normal
+        else:
+            return 'undefined'
+    
+    #returns the equation that is wanted
+    #
+    #type:
+    #Vector(shows dirction vector and a point on a line)
+    #Parametic(shows direct coralation to the points on the line)
+    #Sclar(standard form ONLY IN 2d Space)
+    #
+    def equation(self,typee):
+        if self.space == '2d':
+            if typee == 'Vector':
+                return '(x,y) = '+str(self.point)+' + t'+str(self.dirVector)
+            elif typee == 'Parametic':
+                return str(self.point[0])+' + '+str(self.dirVector.x)+'t',str(self.point[1])+' + '+str(self.dirVector.y)+'t'
+            elif typee == 'Sclar':
+                #print (self.normal)
+                c = -1*(self.point[0]*self.normal.x + self.point[1]*self.normal.y)
+                return str(self.normal.x)+'x'+' + '+str(self.normal.y)+'y + '+str(c)
+        if self.space == '3d':
+            if typee == 'Vector':
+                return '(x,y,y) = '+str(self.point)+' + t'+str(self.dirVector.vec)
+            elif typee == 'Parametic':
+                return str(self.point[0])+' + '+str(self.dirVector.x)+'t',str(self.point[1])+' + '+str(self.dirVector.y)+'t',str(self.point[2])+' + '+str(self.dirVector.z)+'t'
+            elif typee == 'Sclar':
+                return 'does not Exist'
+
+class Plane():
+    
+    #point cords in space (x,y,z)
+    #Two dirVector a given dirction from a point (x,y,z)
+    def __init__(self,point,dirVector1,dirVector2):
+
+        self.point = point
+        self.dirVector1 = Vector(dirVector1[0],dirVector1[1],dirVector1[2])
+        self.dirVector2 = Vector(dirVector2[0],dirVector2[1],dirVector2[2])
+        self.normal = cross(self.dirVector1,self.dirVector2)
+        self.c = -1*(point[0]*self.normal.x + point[1]*self.normal.y + point[2]*self.normal.z)
+
+    def equation(self,typee):
+        if typee == 'Vector':
+                return '(x,y) = '+str(self.point)+' + t'+str(self.dirVector1.vec)+' + s'+str(self.dirVector2.vec)
+        elif typee == 'Parametic':
+                return str(self.point[0])+' + '+str(self.dirVector1.x)+'t'+' + '+str(self.dirVector2.x)+'s',str(self.point[1])+' + '+str(self.dirVector1.y)+'t'+' + '+str(self.dirVector2.y)+'s',str(self.point[2])+' + '+str(self.dirVector1.z)+'t'+' + '+str(self.dirVector2.z)+'s'
+        elif typee == 'Catisian':
+                #print (self.normal)
+                return str(self.normal.x)+'x'+' + '+str(self.normal.y)+'y + '+str(self.normal.z)+'z + '+str(self.c)
+       
+boi = Plane((2,4,5),(7,-8,1),(2,9,-5))
+
+print (boi.equation('Catisian'))
